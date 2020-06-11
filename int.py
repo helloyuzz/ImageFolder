ef712d684505b60129830bca8ed74fe7b091802c
yusais_life@qq.com <39078847+helloyuzz@users.noreply.github.com>
2020-06-11 16:00:41 +0800

    Create int.py

---
+++ int.py
@@ -0,0 +1,64 @@
+## int
+p = 1
+print(p)
+
+## string
+aa = "aa"
+print(aa)
+
+## boolean
+aa = True
+print(aa)
+
+a1 = 2 * 2
+print(a1)
+
+a2 = 4 ** 2
+print(a2)
+
+a3 = 'aa\'a'
+print(a3)
+
+a3 = "aa'a"
+print(a3)
+
+aa = r'aa\a'
+print(aa)
+
+## default break-line
+aaa = """aaa
+        123
+        ddd
+        """
+print(aaa)
+
+## whole-line
+aaa = """
+        aaa\
+        123\
+        ddd\
+    """
+print(aaa)
+
+## link and repeat
+print("aa" + "bb" + 3 * "cc")
+
+## auto link
+print('aa' 'bb')
+
+text = (
+        'askldjflaksdjflaksdfjl;asdjla;f;asdfjalsdfja;lsdfadfja;lskdf_'
+        'fasdflajsdlfjaslkdfjalksdjflaksjfdlkasjfdlasdjflasdjflaksdfj____________'
+       )
+print(text)
+
+##　index
+print("aa"[1])
+
+print("Python"[0:3])
+
+aa = "Python"
+print(aa[:3] + aa[3:])
+
+print(len("Python Applicaiton"))
+print("Python".title())
