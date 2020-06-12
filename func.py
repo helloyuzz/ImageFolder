56ea0c57d370bfdfd875c62e4dbce923237f8e27
yusais_life@qq.com <39078847+helloyuzz@users.noreply.github.com>
2020-06-12 11:09:57 +0800

    Update func.py

--- func.py
+++ func.py
@@ -21,3 +21,20 @@ def aa1(n):
 get_aa1 = aa1(3)
 
 print(get_aa1)
+
+def ask_ok(prompt,times=4,tip="try"):
+    while True:
+        ok = input(prompt)
+        if ok in ('y','o'):
+            return True
+        if ok in ('n','f'):
+            return False
+        times = times - 1
+
+        if times < 0:
+            raise ValueError('invalid value')
+        print(tip)
+
+ask_ok('aa')
+ask_ok('ask_ok',3)
+ask_ok('aaa',3,"aaaaaaaaaaaaaaaaaa")
